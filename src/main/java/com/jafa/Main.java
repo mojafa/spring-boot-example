7a1248e360390f13cc1066d97a12274f5364016b
Jafa Mohammed <jmohsa@gmail.com>
2023-01-11 08:41:19 +0300

    Amigoscode Springboot tutorial

---
+++ src/main/java/com/jafa/Main.java
@@ -0,0 +1,49 @@
+package com.jafa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping("/")
+    public GreetResponse greet() {
+
+        return new GreetResponse("Hello");
+    }
+       record GreetResponse(String greet) {}
+
+    //    record GreetResponse(String greet) {}
+    // OR
+//    public static class GreetResponse {
+//        private String greet;
+//
+//        public GreetResponse(String greet) {
+//            this.greet = greet;
+//        }
+//
+//        public String getGreet() {
+//            return greet;
+//        }
+//
+//        public void setGreet(String greet) {
+//            this.greet = greet;
+//        }
+//        @Override
+//        public String toString() {
+//            return "GreetResponse{" +
+//                    "greet='" + greet + '\'' +
+//                    '}';
+//        }
+//    }
+
+
+}
+
+
